7ce425cbb6fff94ffb3e44875d74094db7c0540a
Krishna Shah <kshahusa@gmail.com>
2025-04-16 13:53:46 -0500

    fixed broadcast mostly

--- src/bin.rs
+++ src/bin.rs
@@ -4,10 +4,7 @@ use std::{sync::{Arc, RwLock}, time::{SystemTime, UNIX_EPOCH}};
 use neuroxide::types::{device::Device, tensor::Tensor, tensordb::{DTypes, TensorDB}};
 use neuroxide::ops::op_generic::Operation;
 use neuroxide::ops::matmul::MatMulOp;
-<<<<<<< HEAD
 use neuroxide::utils::array_utils::broadcast_shapes_matmul;
-=======
->>>>>>> 56a085afa93c430d3cfb31ef1e7ab616c2fd6d65
 use rand::Rng;
 
 #[macro_use]
@@ -23,7 +20,6 @@ extern crate neuroxide;
 fn main() {
     let db = Arc::new(RwLock::new(TensorDB::new(DTypes::I32)));
     let x = Tensor::new(&db,vec![1, 2, 3, 4, 5, 6], vec![1, 2, 3], Device::CPU, true);
-<<<<<<< HEAD
     let x2 = Tensor::new(&db, vec![5, 6, 7, 8, 9, 10], vec![1, 3, 2], Device::CPU, false);
 
     let c = MatMulOp::forward(&vec![&x, &x2]);
@@ -109,56 +105,6 @@ fn main() {
 // }
 
 
-=======
-    let x2 = Tensor::new(&db, vec![5, 6, 7, 8, 9, 10], vec![1, 3, 2], Device::CPU, true);
-
-    let c = MatMulOp::forward(&vec![&x, &x2]);
-    let answer = vec![11, 15, 19, 11, 15, 19];
-    let answer2 = vec![5, 5, 7, 7, 9, 9];
-    let grad = c.backward(Some(vec![x.id, x2.id]));
-    assert_eq!(grad.get(&x.id).unwrap().data, answer);
-    assert_eq!(grad.get(&x.id).unwrap().shape, vec![1, 2, 3]);
-    assert_eq!(grad.get(&x2.id).unwrap().data, answer2);
-    assert_eq!(grad.get(&x2.id).unwrap().shape, vec![1, 3, 2]);
-
-    
-    let x = Tensor::new(&db,vec![1, 2, 3, 4, 5, 6], vec![2, 3], Device::CPU, true);
-    let x2 = Tensor::new(&db, vec![5, 6, 7, 8, 9, 10], vec![3, 2], Device::CPU, true);
-
-    let c = MatMulOp::forward(&vec![&x, &x2]);
-    let answer = vec![11, 15, 19, 11, 15, 19];
-    let answer2 = vec![5, 5, 7, 7, 9, 9];
-    let grad = c.backward(Some(vec![x.id, x2.id]));
-    assert_eq!(grad.get(&x.id).unwrap().data, answer);
-    assert_eq!(grad.get(&x.id).unwrap().shape, vec![2, 3]);
-    assert_eq!(grad.get(&x2.id).unwrap().data, answer2);
-    assert_eq!(grad.get(&x2.id).unwrap().shape, vec![3, 2]);
-
-    let x = Tensor::new(&db,vec![1, 2, 3, 4, 5, 6], vec![6], Device::CPU, true);
-    let x2 = Tensor::new(&db, vec![5, 6, 7, 8, 9, 10], vec![6], Device::CPU, true);
-
-    let c = MatMulOp::forward(&vec![&x, &x2]);
-    let grad = c.backward(None);
-    assert_eq!(grad.get(&x.id).unwrap().data, x2.data);
-    assert_eq!(grad.get(&x.id).unwrap().shape, vec![6]);
-    assert_eq!(grad.get(&x2.id).unwrap().data, x.data);
-    assert_eq!(grad.get(&x2.id).unwrap().shape, vec![6]);
-
-    let x = Tensor::new(&db,vec![1, 2, 3, 4, 5, 6], vec![1, 2, 3], Device::CPU, true);
-    let x2 = Tensor::new(&db, vec![5, 6, 7], vec![3], Device::CPU, true);
-
-    let c = MatMulOp::forward(&vec![&x, &x2]);
-    println!("{:?}", c.shape);
-    // let grad = c.backward(None);
-    // let actual_grad = vec![5, 6, 7, 5, 6, 7];
-    // assert_eq!(grad.get(&x.id).unwrap().shape, vec![1, 2, 3]);
-    // assert_eq!(grad.get(&x.id).unwrap().data, actual_grad);
-    // assert_eq!(grad.get(&x2.id).unwrap().shape, vec![1, 3, 1]);
-    // assert_eq!(grad.get(&x2.id).unwrap().data, vec![5, 7, 9]);
-
-}
-
->>>>>>> 56a085afa93c430d3cfb31ef1e7ab616c2fd6d65
 // fn main() {
 //     // blas_src::blas_set_num_threads(4);
 //     let start = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
